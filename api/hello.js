981ad10371301aeaefb96694f4a1a8915b41a15c
nnafss <sitimulyati.alfatih@gmail.com>
2025-09-22 11:09:43 +0700

    Delete api directory

--- api/hello.js
+++
@@ -1,5 +0,0 @@
-// File: api/hello.js
-export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  return res.status(200).json({ ok: true, msg: "hello from vercel" });
-}
